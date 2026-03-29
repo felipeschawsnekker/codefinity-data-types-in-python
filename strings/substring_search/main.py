985dcc87816ff27e9414885edc1b9f3261b23d59
felipeschawsnekker <>
2026-03-29 17:32:19 +0000

    Substring search

---
+++ strings/substring_search/main.py
@@ -0,0 +1,14 @@
+string = "Work your way up!"
+
+# Find the index of the letter `p`
+p = string.index('p')
+
+# Find the index of the letter `y`
+y = string.index('y')
+
+# Find the index of the letter `r`
+r = string.index('r')
+
+print("Variable p equals:", p)
+print("Variable y equals:", y)
+print("Variable r equals:", r)
